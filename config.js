43061a860c9ea386eb4f2208e2ac83d7a44a6f3b
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-07-29 10:12:24 -0400

    update html

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const CONFIG = {
+    PADLET_ID: 'your-real-padlet-id',
+    PADLET_URL: 'https://padlet.com/your-username/your-title',
+    TEACHABLE_MACHINE_URL: 'https://teachablemachine.withgoogle.com',
+    CERTIFICATE_TEMPLATE: 'certificate-template.pdf',
+    ANALYTICS_ENABLED: true
+};
